9b185f941c07c7eaef9b6ca16ac3eb1a706eb910
Siarhei Stamal <sergei@stml.33mail.com>
2015-04-16 18:04:15 +0300

    need to parse js object(json) + check the user cred.

--- server/appChat.js
+++ server/appChat.js
@@ -1,28 +1,33 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
+var app = require('http').createServer(handler).listen(80),
+    io = require('socket.io')(app),
+    fs = require('fs'),
+    jsonObj = require("./package.json");
+    //xmlhttp = new XMLHttpRequest();
 
-app.listen(80);
+//app.listen(80);
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.on('checkUser', function (data) {
+        //console.log(data);
+        console.log(jsonObj);
+
     });
 })
+
+
 
 function handler (req, res) {
     fs.readFile(__dirname + '/LoginForm.html',
         function (err, data) {
             if (err) {
                 res.writeHead(500);
-                return res.end('Error loading index.html');
+                return res.end('Error loading html file');
             }
 
             res.writeHead(200);
             res.end(data);
         });
 }
-
 
 
 //module.exports = function( app, io ) {
