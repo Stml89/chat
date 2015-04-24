a32ad0920978cbc70a8a7fc5ebd592a3bb98d144
Siarhei Stamal <sergei@stml.33mail.com>
2015-04-24 16:20:16 +0300

    need: 4) dif. colors for dif. users 9) for new users doesn't defined name

--- server/appChat.js
+++ server/appChat.js
@@ -1,45 +1,3 @@
-/*var PORT = 80;
-
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
-var jsonObj = require("./package.json");
-server.listen(PORT);
-
-app.use('/static', express.static(__dirname + '/server'));
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/LoginForm.html');
-});
-
-app.get('/chat', function (req, res) {
-    res.sendfile(__dirname + '/chatForm.html');
-});
-
-io.on('connection', function (socket) {
-    socket.emit('news', {hello: 'world'});
-    socket.on('checkUser', function ( data ) {
-        jsonObj.users.forEach( function ( i ) {
-            if( data.n == i.nickname){
-                i.cred.forEach( function ( y ) {
-                    if( data.p == y.pass ){
-
-                        console.log("OK");
-                        chunk = chunk.replace('/ChatForm.html');
-                        //fs.sendFile(__dirname + '/ChatForm.html');
-                    }/*else{
-                        console.log("username or password is incorrect");
-                    }
-                })
-            }else{
-                console.log("username or password is incorrect");
-            }
-        })
-    })
-})*/
-
 var app = require( 'http' ).createServer( handler ).listen( 80 ),
     io = require( 'socket.io' )( app ),
     fs = require( 'fs' ),
@@ -56,68 +14,47 @@ io.sockets.on('connection', function ( socket ) {
         }
     });
 
-    socket.on('checkUser', function ( data ) {
-        //try {
-        jsonObj.users.forEach( function ( i ) {
-            if( data.n == i.nickname ) {
-                i.cred.forEach( function ( y ) {
-                    if( data.p == y.pass ) {
-                        socket.emit( 'ok' );
-                    }else{
-                     console.log("username or password is incorrect");
-                     }
-                })
-            }else{
-                console.log("username or password is incorrect");
-                //console.log("The username \'" + data.n + "\' is absent or invalid");
+    socket.on( 'checkUser', function ( data ) {
+        for( var i in jsonObj.users ) {
+            if( data.n == jsonObj.users[i].nickname ) {
+                if( data.p == jsonObj.users[i].cred.pass ) {
+                    socket.emit( 'ok' );
+                }else{
+                    console.log("username or password is incorrect");
+                    return false;
+                }
             }
-            //    if( data.n != i.nickname){
-                    //        throw 'throwing exception';
-                    //    }else{
-                    //        i.cred.forEach( function ( y ) {
-                    //            if( data.p == y.pass ){
-                    //                console.log("OK");
-                    //            }
-                    //        })
-                    //    }
-                    //})
-                    //} catch(e){
-                    //    console.log("The User name \'" + data.n + "\' is absent or invalid");
-                    //
-                    //}
-        })
+        }
     })
 
     socket.on( 'addNewUser', function ( data ) {
-        var i = jsonObj.users.length;
-
-//        jsonObj.users.forEach( function ( i ) {
-//            if( data.n != i.nickname ) {
-//                i.cred.forEach( function ( y ) {
-//                    if( data.m != y.email ) {
-                        jsonObj.users[ i ] = {
-                            "nickname": data.n,
-                            "cred": {
-                                "email": data.m,
-                                "pass": data.p
-                            }
+        var y = jsonObj.users.length;
+        //do not work correctly
+        for( var i in jsonObj.users ) {
+            if( data.n != jsonObj.users[i].nickname ) {
+                if( data.m != jsonObj.users[i].cred.email ) {
+                    jsonObj.users[ y ] = {
+                        "nickname": data.n,
+                        "cred": {
+                            "email": data.m,
+                            "pass": data.p
                         }
-//                    }else{
-//                        console.log("Sorry, that email is already taken");
-//                        return false;
-//                    }
-//                })
-//            }else{
-//                console.log("Sorry, that username is already taken");
-//                return false;
-//            }
-//        })
+                    }
 
-        fs.writeFile( 'package.json', JSON.stringify( jsonObj ), function( err ) {
-                if ( err ) return console.log( err );
-            });
+                    fs.writeFile( 'package.json', JSON.stringify( jsonObj ), function( err ) {
+                        if ( err ) return console.log( err );
+                    });
 
-        socket.emit( 'ok' );
+                    socket.emit( 'ok' );
+                }else{
+                    console.log("Sorry, that email is already taken");
+                    return false;
+                }
+            }else{
+                console.log("Sorry, that username is already taken");
+                return false;
+            }
+        }
     })
 })
 
